edbee4d84faad476b2d56a19801d3b549ef8fcd3
Zual Rana <freelancerzual@gmail.com>
2023-04-24 16:15:46 +0600

    get buyer input value

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("done");
+function getInputValueById(id) {
+  const inputField = document.getElementById(id);
+  return inputField.value;
+}
+
+document.getElementById("buyer-button").addEventListener("click", function () {
+  const buyerInputValue = getInputValueById("buyer-input");
+  console.log(buyerInputValue);
+});
